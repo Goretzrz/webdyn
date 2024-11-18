e094c67dfe69a4213b1b2f8e90faafa84c1ae69d
Goretzrz <goretzephirlamarck@gmail.com>
2024-11-18 15:00:26 +0100

    ex 3  reponse en commentaire

---
+++ exercice3/exercice3.js
@@ -0,0 +1,7 @@
+var Mavariablestring ="toto";
+var MavariableNumerique =100;
+var Mavariabletableau =['Apple','Banana'];
+var MarvariableObjet = window
+
+alert(Mavariablestring+ MavariableNumerique + Mavariabletableau[0] + MarvariableObjet.location.pathname + window.innerHeight + window.innerWidth)
+// ajouter var window.innerheight/width dans onglet watch du debugger
