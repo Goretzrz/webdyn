ebb731a0893b230d3f9895fe1fd5f9436c09477b
Goretzrz <goretzephirlamarck@gmail.com>
2024-11-18 13:02:32 +0100

    ex2

---
+++ exercice2/exercice2.js
@@ -0,0 +1,6 @@
+var Mavariablestring ="toto";
+var MavariableNumerique =100;
+var Mavariabletableau =['Apple','Banana'];
+var MarvariableObjet = window
+
+alert(Mavariablestring+ MavariableNumerique + Mavariabletableau[0] + MarvariableObjet.location.pathname + window.innerHeight + window.innerWidth)
