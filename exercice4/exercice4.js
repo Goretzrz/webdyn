2dbd2efc2d4d994d0e5d6dd7c2dc56873cf8740a
Goretzrz <goretzephirlamarck@gmail.com>
2024-11-18 15:08:47 +0100

    ex 4

---
+++ exercice4/exercice4.js
@@ -0,0 +1,12 @@
+var Mavariablestring = "toto";
+var MavariableNumerique = prompt("entrez un nombre",100);
+
+if (Mavariablestring =="toto"   && MavariableNumerique!=200 || MavariableNumerique <= 100) {
+
+    alert ("je suis le cas 1");
+}
+else {
+    alert("je suis le cas 2");
+}
+
+// rentrer le nombre 200 , car dans le code, il y a le !
